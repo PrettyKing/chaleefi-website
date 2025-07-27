f9ab8594e0ebb766da9dd440480cf5d281758b21
chalee <36904775+PrettyKing@users.noreply.github.com>
2025-07-28 02:39:43 +0800

    ⚛️ Add React entry point

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import ChaleefiiWebsite from './ChaleefiiWebsite.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ChaleefiiWebsite />
+  </React.StrictMode>,
+)
